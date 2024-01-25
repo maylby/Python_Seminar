8d84a3406c07fff0cc5759f9a12d5408db7f87be
Vit <mayllby@gmail.com>
2024-01-25 23:33:51 +0300

    Добавил задачу 32

--- lesson07.py
+++ lesson07.py
@@ -25,19 +25,55 @@ an = a1 + (n - 1) * d
 Вариант 1
 """
 
-num = int(input('Input number: ')) 
-step = int(input('Input step: '))
-count = int(input('Input count: '))
+# num = int(input('Input number: ')) 
+# step = int(input('Input step: '))
+# count = int(input('Input count: '))
 
-my_list = [num]
-for i in range(step, count + 1): 
-	my_list.append(num + (i - 1) * step)
+# my_list = [num]
+# for i in range(step, count + 1): 
+# 	my_list.append(num + (i - 1) * step)
 
-print(num, step, count)
-print(*my_list)
+# print(num, step, count)
+# print(*my_list)
 
 '''
 Вариант 2 (генератор списков)
 '''
-my_list = [num + i * step for i in range(count)]
-print(my_list)
+# my_list = [num + i * step for i in range(count)]
+# print(my_list)
+
+
+"""
+Задача 32:
+
+Задать произвольные границы диапазона значений (min, max) и  
+определить индексы элементов массива (списка),
+значения которых лежат в заданном диапазоне (?),
+т.е. не меньше заданного минимума 
+и не больше заданного максимума.
+"""
+
+# Ввод: 
+# [-5, 9, 0, 3, -1, -2, 1,
+# 4, -2, 10, 2, 0, -9, 8, 
+# 10, -9, 0, -5, -5, 7]
+
+# Вывод: [1, 9, 13, 14, 19]
+
+
+#	         1                          9            13  14                19
+list1 = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+#	      0  1  2  3   4   5  6  7   8  9  10  11 12  13 14  15  16 17  18  19
+
+min = int(input('min: '))
+max = int(input('max: '))
+
+def search(arr): # 'arr' - пустой список для передачи значений 'list1'
+	result = []
+	for i in range(len(arr)): 
+		if min <= list1[i] <= max: # допустимая форма записи
+			result.append(i) # добавление индексов элементов
+	return result 
+
+print('Ввод: ', list1)
+print('Вывод:', search(list1)) 
